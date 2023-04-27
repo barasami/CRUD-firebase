c10be836b87231a2cd176f598b302609120dec83
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-27 08:18:43 +0300

    modified form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Form() {
+    const[mytext,setText]=useState('')
+    const submitMe=(e)=>{
+        e.preventDefault()
+    }
   return (
-    <div>Form</div>
+    <div>
+        <div>
+            <form onSubmit={submitMe}>
+                <div>
+                    <input type='text' placeholder='Your Todo...' onChange={(e)=>setText(e.target.value)}/>
+                </div>
+                <div>
+                    <input type='date' onChange={(e)=>setText(e.target.value)}/>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
   )
 }
 
