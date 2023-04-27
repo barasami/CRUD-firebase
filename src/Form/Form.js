4626f0a9f639e42a7a91bf5f77af4f17fe4aeaed
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-27 08:43:24 +0300

    updated todo

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
+import './Form.css'
 
 function Form() {
     const[mytext,setText]=useState('')
     const submitMe=(e)=>{
         e.preventDefault()
+        console.log(mytext);
     }
+
   return (
-    <div>
-        <div>
-            <form onSubmit={submitMe}>
-                <div>
-                    <input type='text' placeholder='Your Todo...' onChange={(e)=>setText(e.target.value)}/>
+    <div className='myform'>
+        <div className='coolform'>
+            <h3 className='heading'>My Todo</h3>
+            <form onSubmit={submitMe} className='form'>
+                <div className='input'>
+                    <input type='text' className='text' placeholder='Your Todo...' onChange={(e)=>setText(e.target.value)}/>
                 </div>
-                <div>
-                    <input type='date' onChange={(e)=>setText(e.target.value)}/>
+                <div className='date'>
+                    <input type='date' className='mydate' onChange={(e)=>setText(e.target.value)}/>
                 </div>
-                <div>
-                    <button>Submit</button>
+                <div className='submit'>
+                    <button className='btn'>Submit</button>
                 </div>
 
             </form>
