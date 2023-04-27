ad1d47b21ba0a352d6825adbedeb8452746bd8c6
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-27 08:08:50 +0300

    modified app

---
+++ src/Fbase/Fbase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnNxJ2VITWaRQirVNu92qglhiXnPWVp3c",
+  authDomain: "fcrud-1fe4e.firebaseapp.com",
+  projectId: "fcrud-1fe4e",
+  storageBucket: "fcrud-1fe4e.appspot.com",
+  messagingSenderId: "401758506576",
+  appId: "1:401758506576:web:64e460aa5c3442f325cb55",
+  measurementId: "G-FMX4Q5E35C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
